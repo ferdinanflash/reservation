66081eeb08ce4363f65fb95b44dae31844b36cb2
ferdinanflash <ferdinan.net@gmail.com>
2026-07-08 03:16:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -519,11 +519,11 @@ const trainSp = parseInt(document.getElementById('input-trainsp').value.trim()) 
     if (!nickname) { showToast("Please enter In-Game Nickname!", "warning"); return; }
     if (!gameId) { showToast("Please enter In-Game ID!", "warning"); return; }
     // VALIDASI: Mencegah input angka minus/negatif
-if (fc < 0) { showToast("Fire Crystals amount cannot be less than 0!", "warning"); return; }
-if (genSp < 0) { showToast("General Speedups cannot be less than 0!", "warning"); return; }
-if (constSp < 0) { showToast("Construction Speedups cannot be less than 0!", "warning"); return; }
-if (resSp < 0) { showToast("Research Speedups cannot be less than 0!", "warning"); return; }
-if (trainSp < 0) { showToast("Training Speedups cannot be less than 0!", "warning"); return; }
+if (fc < 1) { showToast("Fire Crystals amount cannot be less than 0!", "warning"); return; }
+if (genSp < 1) { showToast("General Speedups cannot be less than 0!", "warning"); return; }
+if (constSp < 1) { showToast("Construction Speedups cannot be less than 0!", "warning"); return; }
+if (resSp < 1) { showToast("Research Speedups cannot be less than 0!", "warning"); return; }
+if (trainSp < 1) { showToast("Training Speedups cannot be less than 0!", "warning"); return; }
 
     
     const { error } = await client
